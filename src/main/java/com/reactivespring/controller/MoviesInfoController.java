7348b00873f83d032839c89b12f0813a2a9381f1
dina-gherta <ghertadina@gmail.com>
2024-01-09 16:37:16 +0200

    added new functionality

--- src/main/java/com/reactivespring/controller/MoviesInfoController.java
+++ src/main/java/com/reactivespring/controller/MoviesInfoController.java
@@ -11,7 +11,7 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.core.publisher.Sinks;
 
-import javax.validation.Valid;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/v1")
